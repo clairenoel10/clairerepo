bbd4cc8dc18f7df538351d16148cc674b58f1c17
Claire Noel <47509968+clairenoel10@users.noreply.github.com>
2020-11-16 20:41:40 -0500

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+import java.util.*;
+ 
+public class PrintStatement {
+ public static void main(String[] args){
+   String statement = "Hello World";  //edit text here
+   System.out.println(statement);
+ }
+}
